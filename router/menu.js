37214c1ac88532ad2baa85148a17605d8885e4a9
GitGeecko <133806972+GitGeecko@users.noreply.github.com>
2023-08-31 22:53:53 +0600

    Add files via upload

--- router/menu.js
+++ router/menu.js
@@ -3,6 +3,7 @@ const express=require("express");
 const router=express.Router();
 
 const oracle=require("oracledb");
+const {dbConfig}=require("../connection");
 
 //const{getMenu}=require("../controller/menuController.js");
 //const{getOrder}=require("../controller/menuController.js");
@@ -30,17 +31,45 @@ router.get('/',(req,res)=>{
         }
     );
 });
-router.post("/",(req,res)=>{
+router.post("/",async(req,res)=>{
     const orderedItems=[];
-    for(var i=0;i<items.length;i++){
-        const quantity=parseInt(req.body[items[i][0]]);
-        if(quantity>0){
-            orderedItems.push({name:items[i][1],quantity: quantity});
+    try{
+        const query1='SELECT * FROM DELIVER_MAN ORDER BY DELIVERY_COUNT ASC';
+        const res1= await connection.execute(query1);
+        const deliver_man_id=res1.rows[0][0];
+
+        //const order_id=process.env.order_id;
+        const user_id=process.env.userId;
+        console.log(user_id,'user');
+        const price=0;
+        const review='pp';
+        await connection.execute(
+            "INSERT INTO ORDERS (ORDER_PRICE, REVIEW, USER_ID) VALUES (:price,:review, :user_id)",
+            {price, review, user_id}
+          );
+        const res112=await connection.execute("SELECT ORDER_ID FROM ORDERS WHERE REVIEW='pp' ");
+    
+        //console.log(order_id);
+        const order_id=res112.rows[0][0];
+        console.log(order_id);
+        for(var i=0;i<items.length;i++){
+            const quantity=parseInt(req.body[items[i][0]]);
+            if(quantity>0){
+                orderedItems.push({name:items[i][1],quantity: quantity});
+                const item_no=items[i][0];
+                await connection.execute(
+                    'INSERT INTO ORDER_HISTORY (ITEM_NO,ORDER_ID, COUNT) VALUES (:item_no,:order_id,:quantity)',
+                    {item_no,order_id,quantity}
+                  );
+                  await connection.execute('commit');
+            }
+            //console.log(items[i][1],'Amount',quantities[items[i][1]]);
         }
-        //console.log(items[i][1],'Amount',quantities[items[i][1]]);
-    }
-    //console.log(quantities);
-    res.render('order',{orderedItems});
-});
+        //console.log(quantities);
+        res.render('order',{orderedItems});
+    }catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}); 
 
 module.exports=router;
